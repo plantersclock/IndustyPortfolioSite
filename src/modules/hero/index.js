90ea6918b3caa95aa7bf08217a6b42ae8a178555
Matthew Wright <mattman861@gmail.com>
2021-05-09 18:01:34 -0400

    Lazy Load Header

--- src/modules/hero/index.js
+++ src/modules/hero/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 
 import { useInView } from "react-intersection-observer";
+import LazyLoad from "react-lazyload";
+import { forceCheck } from "react-lazyload";
 
 const Hero = ({ setContactButtonState }) => {
   const { ref, inView } = useInView();
@@ -17,6 +19,7 @@ const Hero = ({ setContactButtonState }) => {
   const anchor = document.querySelector("#contact-form-id");
 
   const width = window.screen.width;
+  forceCheck();
   console.log(width);
 
   return (
@@ -27,11 +30,11 @@ const Hero = ({ setContactButtonState }) => {
             width < 450 ? 450 : 900
           }/v1620583770/IndustryWebsite/LeadingEdgeLogo.png`}
           alt="Leading Edge"
-          className=" w-80 relative z-30 xl:w-1/4 3xl:w-1/5 p-4 md:p-8 2xl:p-10 3xl:ml-20"
+          className=" h-24 xs:h-28 relative z-40 md:h-36 lg:h-44 3xl:h-52 p-4 md:p-8 2xl:p-10 3xl:ml-20"
         />
       </div>
 
-      <div className=" relative z-30 max-w-7xl px-4 mx-auto sm:px-6 lg:px-8 xl:px-20 2xl:px-8">
+      <div className=" relative z-40 max-w-7xl px-4 mx-auto sm:px-6 lg:px-8 xl:px-20 2xl:px-8">
         <div className=" sm:w-4/5 mt-4 sm:mt-4 2xl:mt-12 2xl:w-full">
           <h1 className="sm:font-light text-theme-blue-100 text-3xl xs:text-4xl md:text-6xl 2xl:text-8xl ">
             Aluminum
@@ -45,7 +48,7 @@ const Hero = ({ setContactButtonState }) => {
         </div>
       </div>
 
-      <div className="relative z-30 h-auto w-full mt-20  2xl:mt-28 3xl:mt-72">
+      <div className="relative z-40 h-auto w-full mt-20  2xl:mt-28 3xl:mt-72">
         <div className="absolute top-0 left-0 w-full sm:w-2/3 md:w-1/2  bg-opacity-90 h-full bg-theme-blue-900"></div>
         <div className="relative h-50 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 xl:px-20 2xl:px-8 py-6 xs:py-8 sm:py-10 2xl:py-20 ">
           <h2 className="font-medium text-theme-blue-100 text-2xl md:text-3xl 2xl:text-5xl ">
@@ -68,14 +71,23 @@ const Hero = ({ setContactButtonState }) => {
       </div>
 
       <div
-        className="absolute z-20 top-0 h-full w-full bg-cover"
+        className="absolute z-30 top-0 h-full w-full bg-cover bg-theme-blue-900"
         style={{
-          backgroundImage: ` linear-gradient(to bottom, rgba(10, 15, 33, .5), rgba(10, 15, 33,.5)), url(https://res.cloudinary.com/mpwcloud/image/upload/c_scale,f_auto,q_auto,w_${
-            width + 100
-          }/v1620583770/IndustryWebsite/MetalShavings.png)`,
-          animation: "fadein 1s",
+          backgroundImage: ` linear-gradient(to bottom, rgba(10, 15, 33, .5), rgba(10, 15, 33,.5)), url(https://res.cloudinary.com/mpwcloud/image/upload/c_scale,f_auto,q_auto,w_100/v1620583770/IndustryWebsite/MetalShavings.png)`,
+          // animation: "fadein 1s",
         }}
       ></div>
+      <LazyLoad offset={100}>
+        <div
+          className="absolute z-30 top-0 h-full w-full bg-cover"
+          style={{
+            backgroundImage: ` linear-gradient(to bottom, rgba(10, 15, 33, .5), rgba(10, 15, 33,.5)), url(https://res.cloudinary.com/mpwcloud/image/upload/c_scale,f_auto,q_auto,w_${Math.round(
+              width * 1.5
+            )}/v1620583770/IndustryWebsite/MetalShavings.jpg)`,
+            animation: "fadein .5s",
+          }}
+        ></div>
+      </LazyLoad>
     </div>
   );
 };
